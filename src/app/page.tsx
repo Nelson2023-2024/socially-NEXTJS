6c97498d29861d7a34a5b1f69ef9f92b5d2643f8
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-01-01 02:11:26 +0300

    Shadcn dark and light mode completted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
+import { Button } from "@/components/ui/button";
+import ThemeToggle from "@/components/ui/ThemeToggle";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="m-4">
       <SignedOut>
         <SignInButton mode="modal">
-          <button className="bg-red-500">
-            Sign in
-          </button>
+          <Button>Sign in</Button>
         </SignInButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
+
+      <ThemeToggle/>
     </div>
   );
 }
