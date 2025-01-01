10e7c21b7103cf3a4b55014635238e1f69c6eb68
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-01-01 20:55:17 +0300

    Syncing Clerk with NeonDB to POST the sign in values to NeonDB

--- src/components/Nabar/Nabvar.tsx
+++ src/components/Nabar/Nabvar.tsx
@@ -4,10 +4,16 @@ import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
 import prisma from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
 
 const Nabvar = async() => {
 
   const user = await currentUser()
+
+  //if the user is authenticated sync them to our DB
+  if(user) await syncUser() //POST request
+
+  
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
